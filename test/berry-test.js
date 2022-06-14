d08dc2e46733d2a6bad614a24ac6d636304d700d
alexisljn <alexis.lejeune@gmx.fr>
2022-06-14 15:57:51 +0200

    create test for transfer() and balanceOf()

--- test/berry-test.js
+++ test/berry-test.js
@@ -13,5 +13,22 @@ describe('Berry Contract', () => {
         expect(founderBalance).to.equal(ethers.BigNumber.from("10000000000000000"));
     })
 
-    
+    it('Send from admin to another addr', async () => {
+        const Berry = await ethers.getContractFactory('Berry');
+
+        const berry = await Berry.deploy();
+
+        const [owner, addr1] = await ethers.getSigners();
+        await berry.transfer(addr1.address, ethers.BigNumber.from("1000000000000000"))
+
+        const founderBalance = await berry.balanceOf(owner.address);
+
+        const accountBalance = await berry.balanceOf(addr1.address);
+
+        const supposedFounderBalance = 900000* 10**10;
+        const supposedAccountBalance = 100000* 10**10;
+
+        expect(founderBalance).to.equal(ethers.BigNumber.from(supposedFounderBalance));
+        expect(accountBalance).to.equal(ethers.BigNumber.from(supposedAccountBalance));
+    })
 })
