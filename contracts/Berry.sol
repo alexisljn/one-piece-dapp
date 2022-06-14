9259a169191d2349b533dbc3d1715a4a72bba040
alexisljn <alexis.lejeune@gmx.fr>
2022-06-14 12:22:33 +0200

    Implements IERC20 functions

--- contracts/Berry.sol
+++ contracts/Berry.sol
@@ -1,8 +1,71 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Berry is ERC20 {
+contract Berry is IERC20 {
+    string public name = "Berry";
+    string public symbol = "BER";
+    uint8 public decimals = 10;
+    uint256 public override totalSupply;
+    address public founder;
+    mapping(address => uint) private balances;
+    mapping(address => mapping(address => uint)) private allowed;
+
+    constructor() {
+        founder = msg.sender;
+        totalSupply = 1000000 * (10**decimals); // 1 million at start
+        //_mint(founder, totalSupply);
+    }
+
+    function balanceOf(address account) override external view returns(uint) {
+        return balances[account];
+    }
+
+    function transfer(address to, uint amount) override external returns(bool) {
+        require(balances[msg.sender] >= amount, "Insufficient funds in balance");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+    }
+
+    function allowance(address owner, address spender) override external view returns(uint) {
+        return allowed[owner][spender];
+    }
+
+    function approve(address spender, uint amount) override external returns(bool) {
+        require(balances[msg.sender] >= amount, "Insufficient funds in balance ");
+
+        allowed[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint amount) override external returns(bool) {
+        require(allowed[from][msg.sender] >= amount, "Insufficient funds in allowance");
+
+        allowed[from][msg.sender] -= amount;
+
+        balances[from] -= amount;
+        balances[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+    }
+
+    function _mint(address account, uint amount) internal {
+    }
+
+
+// Implémenter les méthodes de l'interface
+
+
 
 }
