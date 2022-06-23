36b91396b50b6fc6231626ca79b263fe87c86579
alexisljn <alexis.lejeune@gmx.fr>
2022-06-23 19:53:19 +0200

    Use of renderRoutes function in render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ReactElement} from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {Skills} from "./components/pages/Skills/Skills";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 
     type RouteData = {
         path: string,
@@ -44,6 +27,14 @@ function App() {
         ))
     }
 
+    return (
+        <div>
+          Header
+          <Routes>
+              {renderRoutes(routes)}
+          </Routes>
+        </div>
+    );
 }
 
 export default App;
