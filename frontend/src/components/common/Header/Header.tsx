3583efbaccd0440d60097bccb0b311c0e6d39cfd
alexisljn <alexis.lejeune@gmx.fr>
2022-06-24 18:56:30 +0200

    Conditional layout based on user logged or not

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
+import {UserContext} from "../../../App";
+interface HeaderProps {
+    connect: () => void;
+}
 
-export function Header() {
+export function Header({connect}: HeaderProps) {
+
+    const {user} = useContext(UserContext);
+    console.log("val", user)
+
     return (
         <div className="container-fluid bg-dark text-white py-2">
             <div className="row">
                 <div className="col col-lg-11"></div>
                 <div className="col col-lg-1">
-                    <button type="button" className="btn btn-primary">Connexion</button>
+                    {user
+                        ? <p>Connecté !</p>
+                        : <button type="button" className="btn btn-primary" onClick={connect}>Connexion</button>
+                    }
                 </div>
             </div>
         </div>
