d6a4f77fc6ebdcbc78b9236a04e1ee0f448b6538
alexisljn <alexis.lejeune@gmx.fr>
2022-06-24 16:58:08 +0200

    get provider if user has metamask

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Routes} from "react-router-dom";
 import {routes} from "./routes/routes";
 import {renderRoutes} from "./routes/renderRoutes";
 import {Header} from "./components/common/Header/Header";
+import {ethers} from "ethers";
 
 function App() {
+
+    const [provider, setProvider] = useState<ethers.providers.Web3Provider|null>(null);
+
+    useEffect(() => {
+        if (typeof window.ethereum != "undefined") {
+            setProvider(new ethers.providers.Web3Provider(window.ethereum as any)) // https://github.com/MetaMask/providers/issues/200
+            // Message d'erreur si pas de provider à gerer plus tard
+        }
+    }, []);
 
     return (
         <div>
