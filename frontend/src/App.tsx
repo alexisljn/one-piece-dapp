6d3738cd6c965eb046b087d818b1c2182e102b8d
alexisljn <alexis.lejeune@gmx.fr>
2022-06-24 18:09:09 +0200

    Create user state and finalize and set it in connect()

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,29 @@ import {routes} from "./routes/routes";
 import {renderRoutes} from "./routes/renderRoutes";
 import {Header} from "./components/common/Header/Header";
 import {ethers} from "ethers";
+import {Address} from "./types/Address";
 
 function App() {
 
+    const [user, setUser] = useState<Address|null>(null);
     const [provider, setProvider] = useState<ethers.providers.Web3Provider|null>(null);
 
     useEffect(() => {
         if (typeof window.ethereum != "undefined") {
             const web3provider = new ethers.providers.Web3Provider(window.ethereum as any) // https://github.com/MetaMask/providers/issues/200
             setProvider(web3provider)
-            // Message d'erreur à gérer
+            //TODO Message d'erreur à gérer
         }
     }, []);
 
     async function connect() {
-        console.log(await provider?.send("eth_requestAccounts", []));
+        try {
+            const userAddress: Address[] = await provider?.send("eth_requestAccounts", []);
+            setUser(userAddress[0]);
+        } catch (error) {
+            //TODO
+            console.error(error);
+        }
     }
 
     return (
