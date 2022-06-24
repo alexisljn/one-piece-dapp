2b75ae8ab491c9f6927290838b124d64afde47dc
alexisljn <alexis.lejeune@gmx.fr>
2022-06-24 11:12:47 +0200

    remove useless import of React

--- frontend/src/routes/renderRoutes.tsx
+++ frontend/src/routes/renderRoutes.tsx
@@ -1,6 +1,5 @@
 import {RouteData} from "../types/RouteData";
 import {Route} from "react-router-dom";
-import React from "react";
 
 export function renderRoutes(routes: RouteData[]): JSX.Element[] {
     return routes.map((routes, index) => (
