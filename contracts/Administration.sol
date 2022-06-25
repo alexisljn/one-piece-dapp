4d5c7aa44a8b9dded8edb5da96921265770877f5
alexisljn <alexis.lejeune@gmx.fr>
2022-06-25 10:37:29 +0200

    create Administration contract

---
+++ contracts/Administration.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Administration is AccessControl {
+
+    address public founder;
+    bytes32 public constant ROLE_ADMIN = keccak256("ROLE_ADMIN");
+
+    constructor() {
+        founder = msg.sender;
+        _setupRole(keccak256("ROLE_ADMIN"), founder);
+        _setRoleAdmin(keccak256("ROLE_ADMIN"), keccak256("ROLE_ADMIN"));
+    }
+
+    function revokeRoleAdmin(bytes32 role, address account) external {
+        require(msg.sender != founder, "founder will always be admin");
+        require(msg.sender != account, "You cannot revoke yourself your role admin");
+
+        revokeRole(role, account);
+    }
+}
