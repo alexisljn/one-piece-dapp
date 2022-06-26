4cab3cefe0df2a96b52abb9b2ccf8dfc480b6c2d
alexisljn <alexis.lejeune@gmx.fr>
2022-06-26 20:33:48 +0200

    change name of contract's method since they've been renamed

--- test/helpers/AdministrationTestHelper.js
+++ test/helpers/AdministrationTestHelper.js
@@ -1,39 +1,52 @@
+const {keccak256, toUtf8Bytes} = require("ethers/lib/utils");
 
-export const deploy = async () => {
+const deploy = async () => {
     const Administration = await ethers.getContractFactory('Administration');
 
     return await Administration.deploy();
 }
 
-export const addAdmin = async (administrationContract, accountToAdd, thirdPartyAccount = null) => {
+const addAdmin = async (administrationContract, accountToAdd, thirdPartyAccount = null) => {
     if (thirdPartyAccount !== null) {
         const administrationContractToThirdParty = administrationContract.connect(thirdPartyAccount);
 
-        return await administrationContractToThirdParty.grantRole(accountToAdd);
+        return await administrationContractToThirdParty.grantAdmin(accountToAdd);
     }
 
-    return await administrationContract.grantRole(accountToAdd);
+    return await administrationContract.grantAdmin(accountToAdd);
 }
 
-export const removeAdmin = async (administrationContract, accountToRemove, thirdPartyAccount = null) => {
+const removeAdmin = async (administrationContract, accountToRemove, thirdPartyAccount = null) => {
     if (thirdPartyAccount !== null) {
         const administrationContractToThirdParty = administrationContract.connect(thirdPartyAccount);
 
-        return await administrationContractToThirdParty.revokeRole(accountToRemove);
+        return await administrationContractToThirdParty.revokeAdmin(accountToRemove);
     }
 
-    return await administrationContract.revokeRole(accountToRemove);
+    return await administrationContract.revokeAdmin(accountToRemove);
 }
 
-export const isAdmin = async (administrationContract, accountToCheck) => {
-    return await administrationContract.hasRole(accountToCheck);
+const isAdmin = async (administrationContract, accountToCheck) => {
+    return await administrationContract.isAdmin(accountToCheck);
 }
 
 // Our custom role "ROLE_ADMIN" has a role admin to manage it.
-export const getRoleAdminOfRoleAdmin = async (administrationContract) => {
-    return await administrationContract.getRoleAdmin();
+const getRoleAdminOfRoleAdmin = async (administrationContract) => {
+    return await administrationContract.getRoleAdminOfRoleAdmin();
 }
 
-export const renounceRoleAdmin = async (administrationContract) => {
-    return await administrationContract.renounceRole();
+const renounceRoleAdmin = async (administrationContract) => {
+    return await administrationContract.renounceRoleAdmin();
 }
+
+const getRoleAdminHashed = () => {
+    return keccak256(toUtf8Bytes('ROLE_ADMIN'))
+}
+
+exports.deploy = deploy;
+exports.addAdmin = addAdmin;
+exports.removeAdmin = removeAdmin;
+exports.isAdmin = isAdmin;
+exports.getRoleAdminOfRoleAdmin = getRoleAdminOfRoleAdmin;
+exports.renounceRoleAdmin = renounceRoleAdmin;
+exports.getRoleAdminHashed = getRoleAdminHashed;
