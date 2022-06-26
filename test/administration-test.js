50db9f6bdb9bc13dba27b1fe2bdae4bd23335640
alexisljn <alexis.lejeune@gmx.fr>
2022-06-26 18:06:03 +0200

    create administration-test.js

---
+++ test/administration-test.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+
+// Deploiement
+// founder est bien admin (hasRole)
+// Le role pour gerer le role admin est bien role admin
+// Ajout d'un admin
+// Retrait d'un admin
+// Renoncement role
+
+// NEG
+// Retrait d'un admin par lui même
+// Ajout d'un admin par un user non admin
+// Retrait du role admin du founder
+// hasRole d'un user non admin
+
+
+describe('Administration Contract', () => {
+
+    it('', async () => {
+
+    })
+})
