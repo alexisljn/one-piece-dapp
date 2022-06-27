654ef41b257e97ce74a3976e57ba7a95a7e59642
alexisljn <alexis.lejeune@gmx.fr>
2022-06-27 17:17:59 +0200

    Basis of implementation of Administration contract usage in Berry contract

--- contracts/Berry.sol
+++ contracts/Berry.sol
@@ -2,26 +2,50 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "./interfaces/AdministrationInterface.sol";
 import "hardhat/console.sol";
 
 // Mettre des _ pour les attributs/fonctions private et internal
 contract Berry is IERC20 {
+
+    using ERC165Checker for address;
+
     string public name = "Berry";
     string public symbol = "BER";
     uint8 public decimals = 10;
-    uint32 private initialSupply = 1000000;
+    uint32 constant private INITIAL_SUPPLY = 100000000; // Useful ?
     uint256 public override totalSupply;
+    AdministrationInterface administrationContract;
+
     address public founder;
+    bytes32 public constant ROLE_ADMIN = keccak256("ROLE_ADMIN");
+
     mapping(address => uint) private balances;
     mapping(address => mapping(address => uint)) private allowed;
 
-    constructor() {
+    constructor(address administrationContractAddress) {
+
+        require(administrationContractAddress.supportsInterface
+            (
+                type(AdministrationInterface).interfaceId
+            ),
+            'Administration contract does not support Administration interface'
+        );
+
         founder = msg.sender;
-        uint initialSupplyInUnit = initialSupply  * (10 ** decimals);
-        _mint(founder, initialSupplyInUnit); // 1 million at start
+        uint initialSupplyInUnit = INITIAL_SUPPLY  * (10 ** decimals);
+        _mint(founder, initialSupplyInUnit); // 100 million at start
+        administrationContract = AdministrationInterface(administrationContractAddress);
 
         console.log("Deployed by: ", msg.sender);
         console.log("Deployed with supply: %s", initialSupplyInUnit);
+    }
+
+    function testAdmin() public view returns(bool) {
+        require(administrationContract.isAdmin(msg.sender), "Not an admin");
+        return administrationContract.isAdmin(msg.sender);
     }
 
     function balanceOf(address account) override external view returns(uint) {
