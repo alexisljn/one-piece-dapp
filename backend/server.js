5fd38a475e30dec5bb48859c4d2c6d12da31c070
alexisljn <alexis.lejeune@gmx.fr>
2022-06-28 18:45:05 +0200

    Install and initialize cors and body parser

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
