8ee4103f9da590309449fb495dd31361748f2b2c
alexisljn <alexis.lejeune@gmx.fr>
2022-06-29 14:31:06 +0200

    implement siwe backend logic

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const {generateNonceChallenge} = require('./managers/AuthManager');
+const {generateNonceChallenge, challengeNonce} = require('./managers/AuthManager');
+const {SiweMessage, SiweError} = require("siwe");
+const {NonceError} = require('./errors/NonceError');
 
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(bodyParser.json());
@@ -14,7 +16,44 @@ app.get('/', (req, res) => {
 
 app.get('/auth/nonce', (req, res) => {
     console.log('/auth/nonce');
+
     res.json({nonce: generateNonceChallenge()});
+})
+
+app.post('/auth/login', async (req, res) => {
+    try {
+        console.log('/auth/login');
+
+        const {message, signature} = req.body;
+
+        const preparedMessage = new SiweMessage(message)
+        const fields = await preparedMessage.verify({signature});
+
+        const {nonce} = fields.data;
+
+        challengeNonce(nonce);
+
+        // Generate jwt
+
+        res.json({toto: "momo"});
+    } catch (error) {
+        console.log(error); // Log into server
+        if (error.hasOwnProperty('error') && error.error instanceof SiweError) {
+            const {type} = error.error;
+
+            res.status(401);
+            res.json({error: type});
+            return;
+        }
+
+        if (error instanceof NonceError) {
+            res.status(401);
+            res.json({error: error.message});
+        } else {
+            res.status(500);
+            res.json({error: 'Something went wrong'});
+        }
+    }
 })
 
 
