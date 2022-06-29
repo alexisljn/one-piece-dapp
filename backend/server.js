2a263d990f6678cc694a66870a8fbceb1f655ee8
alexisljn <alexis.lejeune@gmx.fr>
2022-06-29 22:29:19 +0200

    use of cleaNonce in /auth/login

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const {generateNonceChallenge, challengeNonce} = require('./managers/AuthManager');
+const {generateNonceChallenge, challengeNonce, generateJwtToken, requireLogin, cleanNonce} = require('./managers/AuthManager');
 const {SiweMessage, SiweError} = require("siwe");
 const {NonceError} = require('./errors/NonceError');
 const {areParametersMissing} = require("./helpers/UtilHelper");
@@ -48,6 +48,7 @@ app.post('/auth/login', async (req, res) => {
         /* Specific handle of SiweError from Siwe package */
         if (error.hasOwnProperty('error') && error.error instanceof SiweError) {
             const {type} = error.error;
+            cleanNonce(req.body.message.nonce);
 
             res.status(401);
             res.json({error: type});
@@ -60,10 +61,12 @@ app.post('/auth/login', async (req, res) => {
                 res.json({error: error.message});
                 break;
             case NonceError:
+                cleanNonce(req.body.message.nonce);
                 res.status(error.status);
                 res.json({error: error.message});
                 break;
             default:
+                cleanNonce(req.body.message.nonce);
                 res.status(500);
                 res.json({error: 'Something went wrong'});
         }
