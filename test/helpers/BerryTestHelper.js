0d54889d68a82e3fd4349fb5ecb2ec969a0273c5
alexisljn <alexis.lejeune@gmx.fr>
2022-07-02 12:25:18 +0200

    create deployBerry function

--- test/helpers/BerryTestHelper.js
+++ test/helpers/BerryTestHelper.js
@@ -1,1 +1,14 @@
 
+const {deployAdministration} = require("./AdministrationTestHelper");
+
+const deployBerry = async () => {
+    const administrationContract = deployAdministration();
+
+    const aggregatorV3Contract = "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e";
+
+    const Berry = await ethers.getContractFactory('Berry');
+
+    return await Berry.deploy(administrationContract.address, aggregatorV3Contract);
+}
+
+exports.deployBerry = deployBerry;
