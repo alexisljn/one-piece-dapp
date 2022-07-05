0ef09cedd9028986d040a3f941c37119f631e19f
alexisljn <alexis.lejeune@gmx.fr>
2022-07-05 15:24:11 +0200

    create PurchaseBerry page component

---
+++ frontend/src/components/pages/PurchaseBerry/PurchaseBerry.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function PurchaseBerry() {
+
+
+
+    return (
+        <div>
+            <p>pURCHASE BERR!Y !</p>
+            <input type="text"/>
+        </div>
+        
+    )
+}
